e34c39e07a661d21fd09902a541c6265a550dc86
Brad <cleggbc@byui.edu>
2020-05-02 18:17:10 -0600

    added index.php

---
+++ web/assignments/index.php
@@ -0,0 +1,20 @@
+<?php
+<!DOCTYPE html>
+<html> 
+    <head>
+        <meta charset="utf-8">
+        <title>Home Page</title>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+        <header>
+            <nav>
+                <a href="home">Home</a>
+                <a class="nav2" href="assighnments">Assignments</a>
+            </nav>
+        </header>
+        <img src="me.jpg" alt="me and my wife"/>
+        <p>Hello eveyone. My name is Brad Clegg and this is my wife Emma Cleeg in the picture above. We got married on January 4th 2020 two days after my birthday. This picture is of us in Jackson Hole Wyoming. One thing I love to do is snowboard. Unfortunatly my wife doesnt ski or snowboard so this was just a site seeing trip. When I used to live in Rexburg Idaho I would snowboard at the Grand Targhee resort. I know live in South Jordan Utah and go to whatever resort I feel like that day. I am currently 23-years-old and am a seinor at BYU-Idaho study web design and development. I am really happy I chose this major because I enjoy making and working on websites. My favorite part about making websites is designing and making a website look the way it does. This is a short paragraph about me and I hope you enjoyed reading it! </p>
+    </body>
+</html>
+?>
