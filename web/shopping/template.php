9252549338fa9317f46949fde189365422962188
Brad <cleggbc@byui.edu>
2020-05-09 16:20:41 -0600

    update

--- web/shopping/template.php
+++ web/shopping/template.php
@@ -9,6 +9,7 @@
         <header>
             <nav>
                 <a href="browse.php">Home</a>
+                <img src="headerimg.png"/>
                 <a href="view.php">View Cart</a>
             </nav>
         </header>
