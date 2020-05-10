243b1d221d1401bdd5333ec9f2a6b69240a44f9f
Brad <cleggbc@byui.edu>
2020-05-09 20:58:06 -0600

    'l'

--- web/shopping/browse.php
+++ web/shopping/browse.php
@@ -13,12 +13,15 @@
     <br>
     <p><?php echo $products[1]["name"]; ?></p>
     <p><?php echo $products[1]["price"]; ?></p>
-    <p><?php echo $products[1]["description"]; ?></p><br>
+    <p><?php echo $products[1]["description"]; ?></p>
+    <a href="addtocart.php?id=2">Add to cart</a><br>
     <p><?php echo $products[2]["name"]; ?></p>
     <p><?php echo $products[2]["price"]; ?></p>
-    <p><?php echo $products[2]["description"]; ?></p><br>
+    <p><?php echo $products[2]["description"]; ?></p>
+    <a href="addtocart.php?id=3">Add to cart</a><br>
     <p><?php echo $products[3]["name"]; ?></p>
     <p><?php echo $products[3]["price"]; ?></p>
     <p><?php echo $products[3]["description"]; ?></p>
+    <a href="addtocart.php?id=4">Add to cart</a>
 </body>
 
