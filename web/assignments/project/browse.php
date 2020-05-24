d8a0be976eed0f681ffc948f8879e6092e6431ff
Brad <cleggbc@byui.edu>
2020-05-23 23:00:45 -0600

    'l'

--- web/assignments/project/browse.php
+++ web/assignments/project/browse.php
@@ -12,10 +12,43 @@ foreach ($db->query('SELECT name, price, description FROM products WHERE id = 1'
 {
   echo $row['name'];
   echo '<br>';
-  echo $row['price'] . 'Ruppees.';
+  echo $row['price'] . ' Ruppees.';
   echo '<br>';
   echo $row['description'];
-}    
+  echo '<br>'
+  echo '<br>'
+}
+    foreach ($db->query('SELECT name, price, description FROM products WHERE id = 2') as $row)
+{
+  echo $row['name'];
+  echo '<br>';
+  echo $row['price'] . ' Ruppees.';
+  echo '<br>';
+  echo $row['description'];
+  echo '<br>'
+  echo '<br>'
+}
+    foreach ($db->query('SELECT name, price, description FROM products WHERE id = 3') as $row)
+{
+  echo $row['name'];
+  echo '<br>';
+  echo $row['price'] . ' Ruppees.';
+  echo '<br>';
+  echo $row['description'];
+  echo '<br>'
+  echo '<br>'
+}
+    foreach ($db->query('SELECT name, price, description FROM products WHERE id = 4') as $row)
+{
+  echo $row['name'];
+  echo '<br>';
+  echo $row['price'] . ' Ruppees.';
+  echo '<br>';
+  echo $row['description'];
+  echo '<br>'
+  echo '<br>'
+}
+    
 ?>
 </body>
 
