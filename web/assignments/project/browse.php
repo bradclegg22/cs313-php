c4ad1247da1b06d7eeef6bc9475aa1e98277038c
Brad <cleggbc@byui.edu>
2020-05-23 19:09:06 -0600

    'l'

--- web/assignments/project/browse.php
+++ web/assignments/project/browse.php
@@ -6,22 +6,6 @@
 ?>
 
 <body>
-    <p><?php echo $products[0]["name"];?></p>
-    <p><?php echo $products[0]["price"];?></p>
-    <p><?php echo $products[0]["description"];?></p>
-    <a href="addtocart.php?id=1">Add to cart</a>
-    <br>
-    <p><?php echo $products[1]["name"]; ?></p>
-    <p><?php echo $products[1]["price"]; ?></p>
-    <p><?php echo $products[1]["description"]; ?></p>
-    <a href="addtocart.php?id=2">Add to cart</a><br>
-    <p><?php echo $products[2]["name"]; ?></p>
-    <p><?php echo $products[2]["price"]; ?></p>
-    <p><?php echo $products[2]["description"]; ?></p>
-    <a href="addtocart.php?id=3">Add to cart</a><br>
-    <p><?php echo $products[3]["name"]; ?></p>
-    <p><?php echo $products[3]["price"]; ?></p>
-    <p><?php echo $products[3]["description"]; ?></p>
-    <a href="addtocart.php?id=4">Add to cart</a>
+
 </body>
 
--- web/assignments/project/products.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $products = array (
-        array("name" => "Master Sword", "price" => "10,000", "description" => "The sacred sword designed to never break and never need sharpening"),
-        array("name" => "Hyrule shield", "price" => "200", "description" => "The Hyrule shild is ment to withstand any attack"),
-        array("name" => "Empty Bottle", "price" => "50", "description" => "Its an empty bottle"),
-        array("name" => "Ocarina", "price" => "1000", "description" => "Said to have magical powers"),
-    );
-?>
