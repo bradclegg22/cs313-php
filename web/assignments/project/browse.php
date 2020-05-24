68579fbb53ad0597f72ded44dc39eb3868f260e8
Brad <cleggbc@byui.edu>
2020-05-23 21:56:28 -0600

    'l'

--- web/assignments/project/browse.php
+++ web/assignments/project/browse.php
@@ -5,6 +5,13 @@
 ?>
 
 <body>
-
+<?php
+foreach ($db->query('SELECT name, price, description FROM products') as $row)
+{
+  echo $row['name'];
+  echo $row['price'] . 'Ruppes.'
+  echo $row['description'];
+}    
+?>
 </body>
 
