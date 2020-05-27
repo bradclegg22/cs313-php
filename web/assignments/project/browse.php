7a3010ff8667f5d91e8d8d8bfbd5247bcf2a6e27
Brad <cleggbc@byui.edu>
2020-05-26 18:18:08 -0600

    'l'

--- web/assignments/project/browse.php
+++ web/assignments/project/browse.php
@@ -8,18 +8,19 @@
 
 <body>
 <?php
-foreach ($db->query('SELECT name, price, description FROM products WHERE id = 1') as $row)
-{
-  echo $row['name'];
-  echo '<br>';
-  echo $row['price'] . ' Ruppees.';
-  echo '<br>';
-  echo $row['description']; 
-  echo '<br>';
-  echo "<a href='view.php'>Add to cart</a>";
-  echo '<br>';
-  echo '<br>';
-}
+$statement = $db->query('SELECT name, price, description FROM products WHERE id = 1');
+$results = $statement->fetchAll(PDO::FETCH_ASSOC);
+$row = $results;
+?>
+    <p><?php echo $row['name'] ?></p>
+    <p><?php echo $row['price'] ?></p>
+    <p><?php echo $row['description'] ?></p>
+<!--<form action="addtocart.php" method"post">
+    <input type="hidden" name="picturenum"/>
+    <input type="submit" name="Submit" value="Submit!" />
+</form> -->
+    <?php
+
     foreach ($db->query('SELECT name, price, description FROM products WHERE id = 2') as $row)
 {
   echo $row['name'];
