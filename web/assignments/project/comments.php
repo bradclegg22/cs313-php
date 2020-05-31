cdce504156da6a13931fd7852abe6135634cb0e9
Brad <cleggbc@byui.edu>
2020-05-30 21:15:30 -0600

    'l'

--- web/assignments/project/comments.php
+++ web/assignments/project/comments.php
@@ -1,14 +1,17 @@
 <?php 
-include('connectdp.php');
-$db = get_db();
 function setComments($db) {
     if(isset($_POST['commentSubmit'])) {
         $userid = $_POST['userid'];
         $date = $_POST['date'];
         $message = $_POST['message'];
         
-        $query = "INSERT INTO comments (userid, date, message) VALUES ('$userid', '$date', '$message')";
+        $query = "INSERT INTO comments (userid, date, message) VALUES (:userid, :date, :message)";
         $statement = $db->prepare($query);
+        
+        $statement->bindValue(':userid', $userid);
+	   $statement->bindValue(':date', $date);
+	   $statement->bindValue(':message', $message);
+        
         $statement->execute();
     }
 }
