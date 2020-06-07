4fb9185526d2f228e6f083622d23ce058dd12a28
Brad <cleggbc@byui.edu>
2020-06-06 21:58:40 -0600

    'l'

--- web/assignments/project/editComment.php
+++ web/assignments/project/editComment.php
@@ -1,17 +1,19 @@
 <body>
     <?php
-         $cid = $_POST['cid'];
-        $date = $_POST['date'];
-        $message = $_POST['message'];
-    
-    require("connectdp.php");
+    include('connectdp.php');
     $db = get_db();
     
+    $statement = $db->prepare('SELECT cid, userid, date, message FROM comments');
+	$statement->execute();
+    
+        while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+    {
     echo "<form method='POST' action='updateComment'>
+    <input type='hidden' name='cid' value='".$row['cid']."'>
     <input type='hidden' name='date' value='".date('Y-m-d')."'>
-    <textarea name='message'></textarea>
+    <textarea name='message='".$row['message']."'></textarea>
     <button type='submit' name='commentSubmit'>Comment</button>
     </form>";
-    header("Location: browse.php");
+        }
     ?>
 </body>
