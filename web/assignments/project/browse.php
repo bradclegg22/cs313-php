09853dba8e62d578c0bb10073ca9d8eaece4982b
Brad <cleggbc@byui.edu>
2020-06-06 22:30:27 -0600

    'l'

--- web/assignments/project/browse.php
+++ web/assignments/project/browse.php
@@ -84,7 +84,6 @@ foreach ($db->query('SELECT name, price, description FROM products WHERE id = 1'
         <input type='hidden' name='date' value='".date('Y-m-d')."'>
         <button type='submit' name='delete'>Delete</button>
         </form>";
-        echo . ' ';
          echo "<form method='POST' action='editComment.php'>
         <button type='submit' name='update'>Update</button>
         </form>";
