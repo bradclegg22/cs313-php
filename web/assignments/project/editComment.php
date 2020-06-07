cb6d168291e88a2a9df4016478ed5e3379706454
Brad <cleggbc@byui.edu>
2020-06-06 21:59:55 -0600

    'l'

--- web/assignments/project/editComment.php
+++ web/assignments/project/editComment.php
@@ -8,7 +8,7 @@
     
         while ($row = $statement->fetch(PDO::FETCH_ASSOC))
     {
-    echo "<form method='POST' action='updateComment'>
+    echo "<form method='POST' action='updateComment.php'>
     <input type='hidden' name='cid' value='".$row['cid']."'>
     <input type='hidden' name='date' value='".date('Y-m-d')."'>
     <textarea name='message='".$row['message']."'></textarea>
